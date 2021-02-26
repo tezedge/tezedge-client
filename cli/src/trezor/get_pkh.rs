c6e6e57a5c2ffa5a0ae842fff497a232f9432780
Zura Benashvili <zura.benashvili@gmail.com>
2021-02-26 15:02:32 +0400

    feat(cli): show error if we receive incorrect address from trezor

--- cli/src/trezor/get_pkh.rs
+++ cli/src/trezor/get_pkh.rs
@@ -1,9 +1,20 @@
+use console::style;
+
 use lib::PublicKeyHash;
 use lib::trezor_api::Trezor;
 
+use crate::common::exit_with_error;
 use super::trezor_execute;
 
 pub fn get_pkh(trezor: &mut Trezor, path: Vec<u32>) -> PublicKeyHash {
-    let address = trezor_execute(trezor.get_address(path));
-    PublicKeyHash::from_base58check(&address).unwrap()
+    let address = trezor_execute(trezor.get_address(path.clone()));
+    match PublicKeyHash::from_base58check(&address) {
+        Ok(pkh) => pkh,
+        Err(_) => {
+            exit_with_error(format!(
+                "invalid public key hash received from trezor: {}",
+                style(&address).red(),
+            ));
+        }
+    }
 }
