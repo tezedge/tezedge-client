d9c109e64834424533b4cccf61812997eca6e887
Zura Benashvili <zura.benashvili@gmail.com>
2021-02-26 09:29:50 +0400

    fix(lib): missing import for NewRevealOperation

--- lib/src/types/operation/mod.rs
+++ lib/src/types/operation/mod.rs
@@ -1,6 +1,9 @@
 mod new_operation;
 pub use new_operation::*;
 
+mod new_reveal_operation;
+pub use new_reveal_operation::*;
+
 mod new_transaction_operation;
 pub use new_transaction_operation::*;
 
