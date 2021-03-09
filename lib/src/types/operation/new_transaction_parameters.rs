9608aeee03a0814325ffa0c78b9862797f809400
Zura Benashvili <zura.benashvili@gmail.com>
2021-03-09 13:03:41 +0400

    feat(lib): implement deserialization for transaction parameters
    required to transfer, delegate from originated local accounts.

--- lib/src/types/operation/new_transaction_parameters.rs
+++ lib/src/types/operation/new_transaction_parameters.rs
@@ -1,4 +1,7 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
+use serde::ser::SerializeMap;
+use serde_json::json;
+use sodiumoxide::hex;
 
 use crate::{Forge, Address, ImplicitAddress};
 use crate::trezor_api::protos::{
@@ -7,7 +10,7 @@ use crate::trezor_api::protos::{
     TezosSignTx_TezosTransactionOp_TezosParametersManager_TezosManagerTransfer,
 };
 
-#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+#[derive(Deserialize, PartialEq, Debug, Clone)]
 pub enum NewTransactionParameters {
     SetDelegate(ImplicitAddress),
     CancelDelegate,
@@ -15,6 +18,90 @@ pub enum NewTransactionParameters {
         to: Address,
         amount: u64,
     },
+}
+
+fn set_delegate_json(addr: &ImplicitAddress) -> serde_json::Value {
+    let delegate = hex::encode(addr.forge().take());
+
+    json!([
+        { "prim": "DROP" },
+        { "prim": "NIL", "args": [{ "prim": "operation" }] },
+        { "prim": "PUSH", "args": [
+            { "prim": "key_hash" },
+            { "bytes": delegate },
+        ] },
+        { "prim": "SOME" },
+        { "prim": "SET_DELEGATE" },
+        { "prim": "CONS" }
+    ])
+}
+
+fn cancel_delegate_json() -> serde_json::Value {
+    json!([
+        { "prim": "DROP" },
+        { "prim": "NIL", "args": [{ "prim": "operation" }] },
+        { "prim": "NONE", "args": [{ "prim": "key_hash" }] },
+        { "prim": "SET_DELEGATE" },
+        { "prim": "CONS" }
+    ])
+}
+
+fn transfer_json(to: &Address, amount: u64) -> serde_json::Value {
+    let amount = amount.to_string();
+
+    match to {
+        Address::Implicit(destination) => json!([
+            { "prim": "DROP" },
+            { "prim": "NIL", "args": [{ "prim": "operation" }] },
+            { "prim": "PUSH", "args": [
+                { "prim": "key_hash" },
+                { "bytes": hex::encode(destination.forge().take()) },
+            ] },
+            { "prim": "IMPLICIT_ACCOUNT" },
+            { "prim": "PUSH", "args": [
+                { "prim": "mutez" },
+                { "int": amount },
+            ] },
+            { "prim": "UNIT" },
+            { "prim": "TRANSFER_TOKENS" },
+            { "prim": "CONS" },
+        ]),
+        Address::Originated(_) => json!([
+            { "prim": "DROP" },
+            { "prim": "NIL", "args": [{ "prim": "operation" }] },
+            { "prim": "PUSH", "args": [
+                { "prim": "address" },
+                { "bytes": hex::encode(to.forge().take()) },
+            ] },
+            { "prim": "CONTRACT", "args": [{ "prim": "unit" }] },
+            [ { "prim": "IF_NONE", "args": [
+                [[{ "prim": "UNIT" }, { "prim": "FAILWITH" }]],
+                [],
+            ] } ],
+            { "prim": "PUSH", "args": [
+                { "prim": "mutez" },
+                { "int": amount },
+            ] },
+            { "prim": "UNIT" },
+            { "prim": "TRANSFER_TOKENS" },
+            { "prim": "CONS" },
+        ])
+    }
+}
+
+impl Serialize for NewTransactionParameters {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+        where S: Serializer,
+    {
+        json!({
+            "entrypoint": "do",
+            "value": match self {
+                Self::SetDelegate(addr) => set_delegate_json(addr),
+                Self::CancelDelegate => cancel_delegate_json(),
+                Self::Transfer { to, amount } => transfer_json(to, *amount),
+            },
+        }).serialize(s)
+    }
 }
 
 impl Into<TezosSignTx_TezosTransactionOp_TezosParametersManager> for NewTransactionParameters {
