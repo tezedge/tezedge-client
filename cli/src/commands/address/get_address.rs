ddd1138bded88b5b3d14bd565cb04a00dee08e0d
Zura Benashvili <zura.benashvili@gmail.com>
2021-03-31 09:01:02 +0400

    feat(cli): integrate getting address from ledger into cli

--- cli/src/commands/address/get_address.rs
+++ cli/src/commands/address/get_address.rs
@@ -1,4 +1,6 @@
+use std::fmt::{self, Display};
 use structopt::StructOpt;
+use console::style;
 
 use lib::ToBase58Check;
 
@@ -14,21 +16,47 @@ pub struct GetAddress {
     #[structopt(short, long, parse(from_occurrences))]
     verbose: u8,
 
-    #[structopt(short = "T", long)]
+    #[structopt(long)]
     trezor: bool,
+
+    #[structopt(long)]
+    ledger: bool,
 
     /// E.g. "m/44'/1729'/0'"
     #[structopt(short, long)]
-    path: String
+    path: String,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub struct NoSourceSpecifiedError;
+
+impl Display for NoSourceSpecifiedError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "source was not specified.\n\n")?;
+        write!(f,
+            "{}: please specify source from which you want to get an address.\n",
+            style("help").yellow(),
+        )?;
+        write!(f, "    - {} to get from Trezor.\n", style("--trezor").bold())?;
+        write!(f, "    - {} to get from Ledger.\n", style("--ledger").bold())
+    }
 }
 
 impl GetAddress {
     pub fn execute(self) -> Result<(), CommandError> {
         let path = parse_derivation_path(&self.path)?;
-        let address = crate::trezor::get_address(
-            &mut crate::trezor::find_device_and_connect(),
-            path,
-        ).to_base58check();
+
+        let address = if self.trezor {
+            crate::trezor::get_address(
+                &mut crate::trezor::find_device_and_connect(),
+                path,
+            ).to_base58check()
+        } else if self.ledger {
+            let ledger = lib::ledger_api::Ledger::connect()?;
+            ledger.get_address(path, false)?.to_base58check()
+        } else {
+            Err(NoSourceSpecifiedError)?
+        };
 
         println!("{}", address);
         Ok(())
